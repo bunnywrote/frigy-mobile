d1957a68338d4f77ba35b91ffd0a91a65924b6f1
Alex <nussa2@bfh.ch>
2018-06-17 17:32:22 +0200

    implemented wait after successful scan.

--- app/src/main/java/org/frigy/frigymobile/Activities/CheckInActivity.kt
+++ app/src/main/java/org/frigy/frigymobile/Activities/CheckInActivity.kt
@@ -5,6 +5,7 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
@@ -27,6 +28,9 @@ class CheckInActivity : AppCompatActivity(), ZBarScannerView.ResultHandler {
     private lateinit var itemBasketRecycler: RecyclerView
     private lateinit var viewAdapter: CheckinBasketItemAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
+
+    private val handler: Handler = Handler()
+
 
     private lateinit var viewModel: CheckinBasketViewModel
 
@@ -69,26 +73,39 @@ class CheckInActivity : AppCompatActivity(), ZBarScannerView.ResultHandler {
         mScannerView.stopCamera()
     }
 
+/*    class ExampleTask(): TimerTask(){
+        override fun run() {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }*/
+
     override fun handleResult(result: Result?) {
         Toast.makeText(this, "Scan successful: " + result?.contents + " (" + result?.barcodeFormat?.name + ")", Toast.LENGTH_SHORT).show()
         val products: LiveData<List<Product>> = ProductRepository(this).searchByBarcode(result?.contents.toString())
 
         products.observe(this, (Observer<List<Product>> {
+            var success: Boolean = false
             if (products.value!!.isEmpty()) {
                 Toast.makeText(this, "No products found for barcode: " + result?.contents, Toast.LENGTH_SHORT).show()
-
             } else if (products.value!!.size == 1) {
                 viewModel.createItemFromProduct(products.value!!.get(0))
+                success = true
             } else {
                 //TODO multiple products found
 /*                for (product in products.value!!) {
                     resultView.append(product.toString())
                 }*/
+                success = true
             }
+
+            if (!success) {
+                mScannerView.resumeCameraPreview(this)
+            } else {
+                handler.postDelayed({ mScannerView.resumeCameraPreview(this) }, 2000)
+            }
+
         }))
 
-        //TODO dont resume camera immediately
-        mScannerView.resumeCameraPreview(this)
     }
 
 }
